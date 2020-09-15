6d4d82d8af3d55d5bfe0928d5fd274a809af1df5
Callum Bryant <callum.l.bryant@gmail.com>
2020-09-16 00:33:35 +1000

    wip strict typing fixes

--- src/components/NodeEditor/rete-components/NumberOutputNode/ReteNumberOutputNode.ts
+++ src/components/NodeEditor/rete-components/NumberOutputNode/ReteNumberOutputNode.ts
@@ -1,8 +1,9 @@
-import Rete from "rete";
+import Rete, {Node} from "rete";
 import Vue, {VueConstructor} from "vue";
 import VueNumControlComponent from "@/components/NodeEditor/rete-components/NumberControl/VueNumberInputControl.vue";
 import ReteNumberInputControl from "@/components/NodeEditor/rete-components/NumberControl/ReteNumberInputControl";
 import numSocket from "@/components/NodeEditor/rete-components/NumSocketType";
+import {NodeData, WorkerInputs, WorkerOutputs} from "rete/types/core/data";
 
 
 class ReteNumberOutputNode extends Rete.Component {
@@ -11,13 +12,13 @@ class ReteNumberOutputNode extends Rete.Component {
         super("Number");
     }
 
-    builder(node) {
-        var out1 = new Rete.Output('num', "Number", numSocket);
+    async builder(node: Node): Promise<void> {
+        const out1 = new Rete.Output('num', "Number", numSocket);
 
-        return node.addControl(new ReteNumberInputControl(this.editor, 'num')).addOutput(out1);
+        node.addControl(new ReteNumberInputControl(this.editor, 'num')).addOutput(out1);
     }
 
-    worker(node, inputs, outputs) {
+    worker(node:NodeData, inputs:WorkerInputs, outputs:WorkerOutputs): void {
         outputs['num'] = node.data.num;
     }
 }
